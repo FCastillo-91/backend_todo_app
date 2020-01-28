3952eb72d45cd3b7ec0fbfeed50253c092a9dcec
Fiona C <fiona.c491@gmail.com>
2020-01-28 15:50:32 +0000

    PUT DELETE POST lambda function working

--- tasks.js
+++ tasks.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
-
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/tasks", function(req, res) {
+app.get("/tasks", function (req, res) {
   res.send({ tasks: ["water plants", "do dishes", "buy oats"] });
+});
+
+app.post("/tasks", function (req, res) {
+  const text = req.body.text;
+  const date = req.body.date;
+
+  res.json({
+    message: `Received a request to add task ${text} with date ${date}`
+  });
+});
+
+app.delete("/tasks/:taskId", function (req, res) {
+  const id = req.params.taskId;
+
+  res.json({
+    message: `Successfully deleted task with ID ${id}`
+  });
+});
+
+app.put("/tasks/:taskId", function (req, res) {
+  const updatedTask = req.body;
+  const id = req.params.taskId; 
+
+  res.json({
+    message: `Successfully updated task ID ${id} with task: ${updatedTask.text}, date: ${updatedTask.date}`
+  });
 });
 
 module.exports.handler = serverless(app);
