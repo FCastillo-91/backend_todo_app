1445a9f1be57eb611523604f3c2c88b51a9e0fc2
Fiona C <fiona.c491@gmail.com>
2020-02-01 16:21:15 +0000

    Credentials safe input implemented, Get and Post endpoints refactored and working

--- tasks.js
+++ tasks.js
@@ -2,22 +2,49 @@ const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/tasks", function (req, res) {
-  res.send({ tasks: ["water plants", "do dishes", "buy oats"] });
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "todos"
 });
 
-app.post("/tasks", function (req, res) {
-  const text = req.body.text;
-  const date = req.body.date;
-
-  res.json({
-    message: `Received a request to add task ${text} with date ${date}`
+app.get("/tasks", function(req, res) {
+  connection.query("SELECT * FROM Tasks", function(err, data) {
+    if (err) {
+      res.status(500).json({
+        error: err
+      })
+    } else {
+      res.status(200).json({
+        todos: data
+      });
+    }
   });
+});
+
+app.post("/tasks", function(req, res) {
+  const newTask = req.body
+  // const newTaskText = req.body.task_Text;
+  // const newTaskDueDate = req.body.due_Date;
+  // const newTaskCompletedValue = req.body.completed;
+
+  connection.query("INSERT INTO Tasks SET ?", [newTask], function(err, data) {
+    if (err) {
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      newTask.taskId = data.insertId;
+      res.status(201).json({newTask});
+    }
+  }); 
 });
 
 app.delete("/tasks/:taskId", function (req, res) {
